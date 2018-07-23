81456566e85a5aef3668413b3d108d62d3c51c2c
yutaatamura <yutaatamura@gmail.com>
2018-07-23 16:32:54 -0500

    Add shoppinglist

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -15,6 +15,46 @@ class ShoppingList extends Component {
 
     render() {
         const { items } = this.state;
+        return(
+            <Container>
+                <Button
+                    color="dark"
+                    style={{marginBottom: '2rem'}}
+                    onClick={() => {
+                        const name = prompt('Enter Item');
+                        if(name) {
+                            this.setState(state => ({
+                                items: [...state.items, { id: uuid(), name}]
+                            }));
+                        }
+                    }}
+                    >
+                    Add Item
+                    </Button>
+
+                    <ListGroup>
+                        <TransitionGroup className="shopping-list">
+                        {items.map(({ id, name }) => (
+                            <CSSTransition key={id} timeout={500} classNames="fade">
+                                <ListGroupItem>
+                                    <Button
+                                        className="remove-btn"
+                                        color="danger"
+                                        size="sm"
+                                        onClick={() => {
+                                            this.setState(state => ({
+                                                items: state.items.filter(item => item.id !== id)
+                                            }));
+                                        }}
+                                        >&times;</Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                        </TransitionGroup>
+                    </ListGroup>
+            </Container>
+        );
     }
 }
 
