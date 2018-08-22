09f8717b659deed95a9cc422399d2d13c844a1d6
yutaatamura <yutaatamura@gmail.com>
2018-08-22 11:52:56 -0500

    Complete basic react and redux application set up

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal'; 
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <AppNavbar />
-        <ShoppingList />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
       </div>
+      </Provider>
     );
   }
 }
